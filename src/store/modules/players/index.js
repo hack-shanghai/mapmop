1d2c6e9f1f92768ac82d1278e9a7da97e69a52b4
Timothé Mermet-Buffet <tim@aliengen.com>
2019-05-18 18:24:31 +0800

    Add validation on player form.

--- src/store/modules/players/index.js
+++ src/store/modules/players/index.js
@@ -18,6 +18,9 @@ const Players = {
     addPlayer({ commit }, player) {
       commit('ADD_PLAYER', player);
     },
+    removePlayer({ commit }, player) {
+      commit('REMOVE_PLAYER', player);
+    },
     setCurrentPlayer({ commit }, player) {
       commit('SET_CURRENT_PLAYER', player);
     },
@@ -25,6 +28,10 @@ const Players = {
   mutations: {
     ADD_PLAYER(state, player) {
       state.players.push(player);
+    },
+    REMOVE_PLAYER(state, player) {
+      let playerIndex = state.players.indexOf(player);
+      state.players.splice(playerIndex, 1);
     },
     SET_CURRENT_PLAYER(state, player) {
       state.currentPlayer = state.players.indexOf(player);
