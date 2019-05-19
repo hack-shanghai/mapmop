c65957e80260c24da6e742cad10d7b8218fde77e
Laurent Erignoux <lerignoux@gmail.com>
2019-05-19 17:14:14 +0800

    Preventing player hand initialisation when player is not defined

--- src/pages/Board.vue
+++ src/pages/Board.vue
@@ -21,9 +21,9 @@
           </div>
         </div>
         <div class="column">
-          <div v-for="(player) in players" :key="player.uuid">
+          <div v-for="(player, index) in players" :key="player.uuid">
             <div v-if="player.uuid != currentPlayer.uuid">
-              <player-insight :player="player"/>
+				      <player-insight :player="player"/>
             </div>
           </div>
         </div>
@@ -31,19 +31,13 @@
     </div>
 
     <div class="container">
-      <player-hand :player="currentPlayer"/>
-    </div>
-
-    <div class="container">
-      <div v-for="card in cards" :key="card.uuid">
-        {{ card.city.name }} - {{ card.pollution }} - {{ card.type }}
-      </div>
+      <player-hand v-if="currentPlayer" :player="currentPlayer"/>
     </div>
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters } from 'vuex';
 import Map from "../components/Map";
 import PlayerHand from "../components/PlayerHand";
 import PlayerInsight from "../components/PlayerInsight";
@@ -51,72 +45,70 @@ import PlayerInsight from "../components/PlayerInsight";
 export default {
   name: "board",
   components: {
-    mapmop: Map,
-    "player-hand": PlayerHand,
-    "player-insight": PlayerInsight
+    'mapmop': Map,
+    'player-hand': PlayerHand,
+		'player-insight': PlayerInsight,
   },
   data() {
     return {
       game: {
-        action_left: 3
-      }
+        action_left: 3,
+      },
     };
   },
   computed: {
     ...mapGetters({
-      players: "players/getPlayers",
-      cities: "board/getCities",
-      connections: "board/getTransitions",
-      init: "board/isInitialized",
-      currentPlayer: "players/getCurrentPlayer",
-      cards: "decks/getCards",
+      players: 'players/getPlayers',
+      cities: 'board/getCities',
+      connections: 'board/getTransitions',
+      init: 'board/isInitialized',
+      currentPlayer: 'players/getCurrentPlayer'
     })
   },
   beforeMount() {
-    if (!this.init) {
-      this.$router.replace("/");
+    if(!this.init) {
+      this.$router.replace('/');
       return;
     }
   },
-  mounted() {},
+  mounted() {
+
+  },
   methods: {
     onMapClick(city) {
       /**
        * If the user is in the city, we decrease a pollution.
        */
-      if (city.uuid == this.currentPlayer.city.uuid) {
+      if(city.uuid == this.currentPlayer.city.uuid) {
         /**
          * If there is no pollution to decrease, we ignore the click.
          */
-        if(!Object.keys(city.pollutions).some((k) => city.pollutions[k] > 0)) {
+        if(city.pollutions.waste == 0 &&
+          city.pollutions.nuclear == 0 &&
+          city.weather == 0) {
           return;
         }
 
-        this.$store.dispatch('board/decreasePollution', city)
-        .then(() => {
-          this.decreaseAction();
-        });
+        // TODO: Decreate a pollution.
 
+        this.decreaseAction();
         return;
       }
 
        /**
         * If the user is in the city close to the one clicked, we move the user.
         */
-        this.$store.dispatch('players/moveToCity', city)
-        .then(() => {
-          this.decreaseAction();
-        }).catch(() => {
-          // TODO: Display message, cannot move there.
-        });
+        // TODO: Create the move.
+
+        this.decreaseAction();
     },
     decreaseAction() {
       this.game.action_left--;
 
       if(this.game.action_left < 1) {
-        // TODO: End the turn, and switch to the next player.
+        // TODO: End the round, and switch to the next player.
       }
-    }
-  }
+    },
+  },
 };
 </script>
