4d31ecf5bc906347ffb015721efbd8e9adb904a8
Laurent Erignoux <lerignoux@gmail.com>
2019-05-19 13:09:43 +0800

    Adding missing players hands components

---
+++ src/components/PlayerHand.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="player-hand">
+    <h1>{{ player.name }}</h1>
+    <div class="columns">
+    	<div class="column" v-for="(card, index) in player.cards">
+        <city-card :card="card" :key="card.uuid"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlayerHand',
+  props: {
+    player: Object
+  },
+  methods: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
