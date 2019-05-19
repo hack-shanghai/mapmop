d751ee765ed5bdcb19d283c4c0520b76df8346ed
Laurent Erignoux <lerignoux@gmail.com>
2019-05-19 13:18:25 +0800

    Fixing dirty stuff pushed on master...

--- src/components/PlayerInsight.vue
+++ src/components/PlayerInsight.vue
@@ -2,8 +2,8 @@
   <div class="player-insight">
     <h1>{{ player.name }}</h1>
     <div class="columns">
-    	<div class="column" v-for="(card, index) in player.cards">
-        <city-card :card="card" :key="card.uuid" small="true"/>
+      <div class="column" v-for="card in player.cards" :key="card.uuid">
+        <city-card :card="card" small="true"/>
       </div>
     </div>
   </div>
