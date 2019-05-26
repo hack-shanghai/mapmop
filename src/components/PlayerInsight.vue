ec6276d4f68eddea56af7aae58ec6de9077278ca
Laurent Erignoux <lerignoux@gmail.com>
2019-05-26 18:37:23 +0800

    Adding player avatar in other players view

--- src/components/PlayerInsight.vue
+++ src/components/PlayerInsight.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="player-insight">
-    <h1>{{ player.name }}</h1>
+    <h1><div class="player-avatar-mask" :style="{'background-color': getPlayerColor}"></div>{{ player.name }}</h1>
     <div class="columns">
       <div class="column" v-for="card in player.cards" :key="card.uuid">
         <city-card :card="card" :small="true"/>
@@ -10,16 +10,33 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'PlayerInsight',
   props: {
     player: Object
   },
   methods: {
-  }
+  },
+  computed: {
+      ...mapGetters({
+        characters: 'config/getCharacters'
+      }),
+      getPlayerColor() {
+        return this.characters[this.player.character].color;
+      }
+    },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.player-avatar-mask {
+  margin-right: 2px;
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  mask-image: url(/avatar.svg);
+}
 </style>
